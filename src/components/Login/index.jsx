16d51a71b4baed34a955f3cf0a00107b1bcb2719
Pranav Bakre <pranavbakre@yahoo.in>
2021-02-04 00:31:14 +0530

    Passwords are meant to be hidden
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -80,6 +80,7 @@ class Login extends React.Component {
         />
         <input
           name="password"
+          type="password"
           onChange={this.handleInputChange}
           onBlur={this.validateField}
         />
