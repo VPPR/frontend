18a32acb44a47c58f62d019a73c80f1d4b444e2f
Pranav Bakre <pranavbakre@yahoo.in>
2021-02-13 14:52:42 +0530

    unnecessary imports removed
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -7,8 +7,6 @@ import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import { NavLink } from "react-router-dom";
 const drawerWidth = 240;
 
