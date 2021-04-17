219830c0a9d1c1f7cbccbb0ba8208651052eabbd
Pranav Bakre <pranavbakre@yahoo.in>
2021-04-17 21:45:03 +0530

    Dashboard component is mounted on / now
    formatting
    
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -9,62 +9,58 @@ import Band from "./routes/band";
 import AuthenticatedRoute from "components/AuthenticatedRoute";
 import Users from "./routes/users";
 const style = (theme) => ({
-  fullScreen: {
-    display: "flex",
-    height: "100vh",
-    width: "100vw",
-  },
-  toolbar: theme.mixins.toolbar,
-  content: {
-    padding: theme.spacing(3),
-    height: "100%",
-    width: "100%",
-  },
+    fullScreen: {
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        padding: theme.spacing(3),
+        height: "100%",
+        width: "100%",
+    },
 });
 
 class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
+
+    handleDrawer = () => {
+        this.setState({ open: !this.state.open });
     };
-  }
 
-  handleDrawer = () => {
-    this.setState({ open: !this.state.open });
-  };
-
-  render() {
-    const { classes, is_admin } = this.props;
-    return (
-      <Grid className={classes.fullScreen}>
-        <Header handleDrawer={this.handleDrawer} />
-        <Sidebar open={this.state.open} routes={allowedRoutes(is_admin)} />
-        <div
-          style={{
-            flexShrink: 1,
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <div className={classes.toolbar} />
-          <div
-            className={classes.content}
-          >
-            <Switch>
-              <AuthenticatedRoute path="/dashboard/upload" component={Band} />
-              <AuthenticatedRoute path="/dashboard/users" component={Users} />
-            </Switch>
-          </div>
-        </div>
-      </Grid>
-    );
-  }
+    render() {
+        const { classes, is_admin } = this.props;
+        return (
+            <Grid className={classes.fullScreen}>
+                <Header handleDrawer={this.handleDrawer} />
+                <Sidebar open={this.state.open} routes={allowedRoutes(is_admin)} />
+                <div
+                    style={{
+                        flexShrink: 1,
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <div className={classes.toolbar} />
+                    <div className={classes.content}>
+                        <Switch>
+                            <AuthenticatedRoute path="/upload" component={Band} />
+                            <AuthenticatedRoute path="/users" component={Users} />
+                        </Switch>
+                    </div>
+                </div>
+            </Grid>
+        );
+    }
 }
 const mapStateToProps = (state) => ({
-  is_admin: state.user.currentUser ? state.user.currentUser.is_admin : false,
+    is_admin: state.user.currentUser ? state.user.currentUser.is_admin : false,
 });
-export default withRouter(
-  withStyles(style)(connect(mapStateToProps)(Dashboard)),
-);
+export default withRouter(withStyles(style)(connect(mapStateToProps)(Dashboard)));
