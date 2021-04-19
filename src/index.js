b1407c2211be776f6c7e9b96ce08f5cf0475e85d
Pranav Bakre <pranavbakre@yahoo.in>
2021-04-19 18:47:09 +0530

    default dashboard page ui
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/index.js
+++ src/index.js
@@ -9,81 +9,75 @@ import store from "redux/store";
 import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 
 const lightTheme = createMuiTheme({
-  palette: {
-    type: "light",
-    background: {
-      default: "#d5d5d5",
-      paper: "#ffffff",
-      header: "#1e1e1e",
-      sidebar: "#1e1e1e"
-    },
-    text: {
-      sidebar: "#ffffff"
-    }
-  },
-  overrides: {
-    MuiDataGrid: {
-      root: {
-        border: "1px solid #626262",
-        "& .MuiDataGrid-columnsContainer": {
-          borderBottom: "1px solid #626262"
+    palette: {
+        type: "light",
+        background: {
+            paper: "#ffffff",
+            header: "#1e1e1e",
+            sidebar: "#1e1e1e",
         },
-        "& .MuiDataGrid-cell": {
-          borderBottom: "1px solid #626262"
+        text: {
+            sidebar: "#ffffff",
         },
-        "& .MuiDataGrid-columnSeparator": {
-          color: "#626262"
-        }
-      },
     },
-
-
-
-  },
-  styles: {
-    centerItem: {
-      marginLeft: "auto",
-      marginRight: "auto",
+    overrides: {
+        MuiDataGrid: {
+            root: {
+                border: "1px solid #626262",
+                "& .MuiDataGrid-columnsContainer": {
+                    borderBottom: "1px solid #626262",
+                },
+                "& .MuiDataGrid-cell": {
+                    borderBottom: "1px solid #626262",
+                },
+                "& .MuiDataGrid-columnSeparator": {
+                    color: "#626262",
+                },
+            },
+        },
     },
-  }
-})
+    styles: {
+        centerItem: {
+            marginLeft: "auto",
+            marginRight: "auto",
+        },
+    },
+});
 
 const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-    background: {
-      default: "#0c1112", //"#2a2d36",//"#0e1017", //
-      paper: "#202829",
-      header: "#14191a", //"#383c48"//"#171922"//
+    palette: {
+        type: "dark",
+        background: {
+            default: "#0c1112", //"#2a2d36",//"#0e1017", //
+            paper: "#202829",
+            header: "#14191a", //"#383c48"//"#171922"//
+        },
+        primary: {
+            main: "#1a6bab",
+        },
+        text: {
+            sidebar: "#ffffff",
+        },
     },
-    primary: {
-      main: "#1a6bab",
+    styles: {
+        centerItem: {
+            marginLeft: "auto",
+            marginRight: "auto",
+        },
     },
-    text: {
-      sidebar: "#ffffff"
-    }
-  },
-  styles: {
-    centerItem: {
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
-  },
 });
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root"),
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={window.matchMedia("(prefers-color-scheme: dark)").matches ? darkTheme : lightTheme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
