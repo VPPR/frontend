bf34f51007522608db5869449fd0ed22719c6068
Pranav Bakre <pranavbakre@yahoo.in>
2021-04-22 22:46:24 +0530

    FIX: Upload api was being called twice, reason: same saga was imported and added twice
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,38 +4,32 @@ import createFilter from "redux-persist-transform-filter";
 import AuthActionTypes from "redux/auth/action.type";
 import authReducer from "redux/auth/reducer";
 import userReducer from "redux/users/reducer";
-import bandReducer from "./band/reducer";
-import uploadReducer from './band/reducer';
+import uploadReducer from "./band/reducer";
 
-const authFilter = createFilter("auth", [
-  "accessToken",
-  "expiry",
-  "isLoggedIn",
-]);
+const authFilter = createFilter("auth", ["accessToken", "expiry", "isLoggedIn"]);
 
 const persistConfig = {
-  key: "vppr",
-  storage: storage,
-  whitelist: ["auth"],
-  transforms: [authFilter],
+    key: "vppr",
+    storage: storage,
+    whitelist: ["auth"],
+    transforms: [authFilter],
 };
 
 const mainReducer = persistCombineReducers(persistConfig, {
-  auth: authReducer,
-  user: userReducer,
-  band: bandReducer,
-  upload: uploadReducer
+    auth: authReducer,
+    user: userReducer,
+    upload: uploadReducer,
 });
 
 const rootReducer = (state, action) => {
-  if (action.type === AuthActionTypes.LOGOUT) {
-    storage.removeItem("persist:vppr");
-    const persist = { ...state._persist };
-    state = {};
-    state._persist = persist;
-  }
+    if (action.type === AuthActionTypes.LOGOUT) {
+        storage.removeItem("persist:vppr");
+        const persist = { ...state._persist };
+        state = {};
+        state._persist = persist;
+    }
 
-  return mainReducer(state, action);
+    return mainReducer(state, action);
 };
 
 export default rootReducer;
