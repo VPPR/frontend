30f64dc54be1d941e8e131bc3bb4696e834027e3
Pranav Bakre <pranavbakre@yahoo.in>
2021-05-02 21:03:48 +0530

    validations on signup
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/routes/Signup/index.jsx
+++ src/routes/Signup/index.jsx
@@ -5,6 +5,8 @@ import { signup } from "redux/auth/action";
 import { Button, CircularProgress, FormControlLabel, Grid, Paper, Switch, TextField } from "@material-ui/core";
 import { toast } from "react-toastify";
 
+const emailRegex = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,10}$/i);
+const passwordRegex = RegExp(/^(.{0,7})|([^0-9]*)|([^A-Z]*)|([^a-z]*)$/);
 class Signup extends React.Component {
     constructor(props) {
         super(props);
@@ -15,12 +17,7 @@ class Signup extends React.Component {
             password: "",
             phone: "",
             is_admin: false,
-            errors: {
-                fullname: "",
-                email: "",
-                password: "",
-                phone: "",
-            },
+            errors: {},
         };
     }
 
@@ -45,17 +42,46 @@ class Signup extends React.Component {
         });
     };
 
+    handleValidation = (e) => {
+        const { name, value } = e.target;
+        let errors = { ...this.state.errors };
+        switch (name) {
+            case "email":
+                errors[name] = value === "" || emailRegex.test(value) ? "" : "Invalid Email";
+                break;
+            case "password":
+                errors[name] =
+                    value !== "" && passwordRegex.test(value)
+                        ? "Invalid Password. Password must contain a capital letter, a small letter, numbers and be of minimum length 8"
+                        : "";
+                break;
+            case "phone":
+                errors[name] = value !== "" && /^[6,7,8,9].[0-9]{6,9}$/.test(value) ? "" : "Invalid phone number";
+                break;
+            default:
+                break;
+        }
+        this.setState({ errors }, () => {
+            console.log(this.state);
+        });
+    };
+
     handleSubmit = (e) => {
         e.preventDefault();
-        const { fullname, email, password, phone, is_admin } = this.state;
-
-        this.props.signup({
-            fullname,
-            email,
-            password,
-            phone,
-            is_admin,
-        });
+        const { fullname, email, password, phone, is_admin, errors } = this.state;
+        if (!errors)
+            this.props.signup({
+                fullname,
+                email,
+                password,
+                phone,
+                is_admin,
+            });
+        else {
+            toast.error("Yo! WTF. Fix your mistakes", {
+                position: toast.POSITION.TOP_CENTER,
+            });
+        }
     };
 
     render() {
@@ -74,29 +100,42 @@ class Signup extends React.Component {
                         onChange={this.handleInputChange}
                         value={this.state.fullname}
                         style={{ marginTop: "0.5rem" }}
+                        required
                     ></TextField>
                     <TextField
                         name="email"
                         label="Email ID"
                         onChange={this.handleInputChange}
+                        onBlur={this.handleValidation}
+                        error={!!this.state.errors.email}
+                        helperText={this.state.errors.email}
                         value={this.state.email}
                         style={{ marginTop: "0.5rem" }}
+                        required
                     ></TextField>
                     <TextField
                         name="password"
                         type="password"
                         label="Password"
                         onChange={this.handleInputChange}
+                        onBlur={this.handleValidation}
+                        error={!!this.state.errors.password}
+                        helperText={this.state.errors.password}
                         value={this.state.password}
                         style={{ marginTop: "0.5rem" }}
+                        required
                     ></TextField>
                     <TextField
                         name="phone"
                         type="phone"
                         label="Phone Number"
                         onChange={this.handleInputChange}
+                        onBlur={this.handleValidation}
+                        error={!!this.state.errors.phone}
+                        helperText={this.state.errors.phone}
                         value={this.state.phone}
                         style={{ marginTop: "0.5rem" }}
+                        required
                     />
                     <FormControlLabel
                         style={{ marginTop: "0.5rem" }}
