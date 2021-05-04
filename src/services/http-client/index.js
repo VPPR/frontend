ea758e2d67b6b8a630c7bee99f4fbd7fccaefce4
Pranav Bakre <pranavbakre@yahoo.in>
2021-05-04 22:12:48 +0530

    Fixed error message
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/services/http-client/index.js
+++ src/services/http-client/index.js
@@ -1,32 +1,27 @@
-import { call, put, select } from 'redux-saga/effects';
-import { logout } from 'redux/auth/action';
+import { call, put, select } from "redux-saga/effects";
+import { logout } from "redux/auth/action";
 
 const httpClient = async (url, parameters) =>
-  await fetch(`${process.env.REACT_APP_BACKEND}${url}`, parameters).then(
-    async (response) => {
-      const json = await response.json();
-      return response.ok ? json : Promise.reject(json);
-    },
-  );
+    await fetch(`${process.env.REACT_APP_BACKEND}${url}`, parameters).then(async (response) => {
+        const json = await response.json();
+        return response.ok ? json : Promise.reject(json);
+    });
 
 export default httpClient;
 
 export function* APICall(url, parameters) {
-    const { accessToken,expiry } = yield select(state => state.auth);
+    const { accessToken, expiry } = yield select((state) => state.auth);
 
-    if (new Date(expiry) - new Date()<30000) {
-      let error = new Error();
-      error.detail=`${new Date(expiry) - new Date()}`;
-      yield put(logout())
-      throw(error);
+    if (new Date(expiry) - new Date() < 30000) {
+        let error = new Error();
+        error.detail = "Session expired. Please login again";
+        yield put(logout());
+        throw error;
     }
 
+    if (!parameters.headers) parameters.headers = {};
 
+    parameters.headers.Authorization = `Bearer ${accessToken}`;
 
-    if (!parameters.headers)
-      parameters.headers={}
-    
-    parameters.headers.Authorization=`Bearer ${accessToken}`;
-
-    return yield call(httpClient, url, parameters)
+    return yield call(httpClient, url, parameters);
 }
