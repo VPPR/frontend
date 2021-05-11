1e7e7dbddc01d127225e72e5785719e91d732c02
Pranav Bakre <pranavbakre@yahoo.in>
2021-05-11 23:28:37 +0530

    finally centered icons on sidebar
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -18,6 +18,9 @@ const useStyles = makeStyles((theme) => ({
     },
     component: {
         color: theme.palette.text.sidebar,
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(1),
+        },
     },
     hide: {
         display: "none",
